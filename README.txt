02c7df953b98d012ac2ab7ed62396465f29113ba
J <jofuentes@est.frba.edu.ar>
2020-10-20 00:51:25 -0300

    Katakana dataset load

--- README.txt
+++ README.txt
@@ -2,3 +2,5 @@ Instalar Python 3.8.2 de 64 bits
 python -m pip install --upgrade pip
 pip install Keras
 pip install --upgrade tensorflow
+pip install scikit-image
+pip install sklearn
--- katakana.py
+++ katakana.py
@@ -4,6 +4,14 @@ from keras.layers import Dropout
 from keras.utils import np_utils
 from keras.datasets import mnist
 
+import struct
+from PIL import Image
+import numpy as np
+
+
+import skimage.transform
+from sklearn.model_selection import train_test_split
+
 def usage():
     print('''
     Usage: 
@@ -17,6 +25,40 @@ def main():
 #    if len(sys.argv) < 4:
 #        usage()
 #        exit(1)
+
+
+    # Cargar datasets ETL1C-07 a ETL1C-13
+    read_kana()
+
+    # Transformar
+    kana = np.load("kana.npz")['arr_0'].reshape([-1, 63, 64]).astype(np.float32)
+    kana = kana/np.max(kana) # make the numbers range from 0 to 1
+
+    # 51 is the number of different katakana (3 are duplicates so in the end there are 48 classes), 1411 writers.
+    train_images = np.zeros([51 * 1411, 48, 48], dtype=np.float32)
+
+    for i in range(51 * 1411): # change the image size to 48*48
+        train_images[i] = skimage.transform.resize(kana[i], (48, 48))
+
+    arr = np.arange(51) # create labels
+    train_labels = np.repeat(arr, 1411)
+
+    # In the actual code, I combined the duplicate classes here and had 48 classes in the end
+
+    # split the images/labels to train and test
+    train_images, test_images, train_labels, test_labels = train_test_split(train_images, train_labels, test_size=0.2)
+
+    np.savez_compressed("katakana_train_images.npz", train_images)
+    np.savez_compressed("katakana_train_labels.npz", train_labels)
+    np.savez_compressed("katakana_test_images.npz", test_images)
+    np.savez_compressed("katakana_test_labels.npz", test_labels)    
+        
+    
+        
+    
+    
+    
+    
     
     # load data
     (X_train, y_train), (X_test, y_test) = mnist.load_data()    
@@ -38,13 +80,13 @@ def main():
 
     # define baseline model
     def baseline_model():
-	    # create model
-	    model = Sequential()
-	    model.add(Dense(num_pixels, input_dim=num_pixels, kernel_initializer='normal', activation='relu'))
-	    model.add(Dense(num_classes, kernel_initializer='normal', activation='softmax'))
-	    # Compile model
-	    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-	    return model
+        # create model
+        model = Sequential()
+        model.add(Dense(num_pixels, input_dim=num_pixels, kernel_initializer='normal', activation='relu'))
+        model.add(Dense(num_classes, kernel_initializer='normal', activation='softmax'))
+        # Compile model
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        return model
         
     # build the model
     model = baseline_model()
@@ -55,6 +97,31 @@ def main():
     print("Baseline Error: %.2f%%" % (100-scores[1]*100))        
         
         
+def read_record_ETL1G(f):
+    s = f.read(2052)
+    r = struct.unpack('>H2sH6BI4H4B4x2016s4x', s)
+    iF = Image.frombytes('F', (64, 63), r[18], 'bit', 4)
+    iL = iF.convert('P')
+    return r + (iL,)
+
+def read_kana():
+    katakana = np.zeros([51, 1411, 63, 64], dtype=np.uint8) # 51 characters, 1411 writers, img size = 63*64
+    for i in range(7,14):
+        filename = 'ETL1/ETL1C_{:02d}'.format(i)
+        with open(filename, 'rb') as f: # file 13 only has 3 characters, others have 8 characters
+            if i!=13: limit = 8
+            else: limit=3
+            for dataset in range(limit):
+                for j in range(1411):
+                    try :
+                        r = read_record_ETL1G(f)
+                        katakana[(i - 7) * 8 + dataset, j] = np.array(r[-1])
+                    except struct.error: # two imgs are blank according to the ETL website, so this prevents any errors
+                        pass
+    np.savez_compressed("kana.npz", katakana)
+    
+    
+
         
 
 if __name__ == "__main__":

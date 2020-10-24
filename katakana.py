30c73eaee064ac6d717ca5c10b40989813b06c67
J <jofuentes@est.frba.edu.ar>
2020-10-24 15:42:33 -0300

    Pruebas

--- katakana.py
+++ katakana.py
@@ -49646,16 +49646,20 @@ def main():
     1, 1, 1, 1, 1, 1, 1, 1]
 
 
-    train_images=[c032, c033, c034, c035, c036, c037, c038, c039, c040, c041, c042, c043, c044, c045, c046, c047, 
-    c048, c049, c050, c051, c052, c053, c054, c055, c056, c057, c058, c059, c060, c061, c062, c063, 
-    c064, c065, c066, c067, c068, c069, c070, c071, c072, c073, c074, c075, c076, c077, c078, c079, 
-    c080, c081, c082, c083, c084, c085, c086, c087, c088, c089, c090, c091, c092, c093, c094, c095, 
-    c096, c097, c098, c099, c100, c101, c102, c103, c104, c105, c106, c107, c108, c109, c110, c111, 
-    c112, c113, c114, c115, c116, c117, c118, c119, c120, c121, c122, c123, c124, c125, c126, c127]
+#    train_images=[c032, c033, c034, c035, c036, c037, c038, c039, c040, c041, c042, c043, c044, c045, c046, c047, 
+#    c048, c049, c050, c051, c052, c053, c054, c055, c056, c057, c058, c059, c060, c061, c062, c063, 
+#    c064, c065, c066, c067, c068, c069, c070, c071, c072, c073, c074, c075, c076, c077, c078, c079, 
+#    c080, c081, c082, c083, c084, c085, c086, c087, c088, c089, c090, c091, c092, c093, c094, c095, 
+#    c096, c097, c098, c099, c100, c101, c102, c103, c104, c105, c106, c107, c108, c109, c110, c111, 
+#    c112, c113, c114, c115, c116, c117, c118, c119, c120, c121, c122, c123, c124, c125, c126, c127]
 
     # train_images=[c032, c033, c034, c035, c036, c037, c038, c039, c040, c041, c042, c043, c044, c045, c046]
     # train_images=[c032, c033, c034, c035, c036, c037, c038, c039]
     #train_images=[c066, c067]
+
+    train_images=[c066, c067, c068, c069,c066, c067, c068, c069,c066, c067, c068, c069,c066, c067, c068, c069,c066, c067, c068, c069]
+
+
 
     # Size of image(width)
     n_side = 64
@@ -49812,10 +49816,10 @@ def test(weights, testing_data):
 
     for data in testing_data:
         true_data = data[0]
-        noisy_data = data[1]
+        noisy_data = convert_bipolar(data[1])
         
         # Utilizando la matriz W, ingreso con el pattern a reconocer
-        #predicted_data = convert_zero(retrieve_pattern(weights, noisy_data))
+        #predicted_data = retrieve_pattern(weights, noisy_data)
         predicted_data = convert_zero(retrieve_pattern(weights, noisy_data))
         
         # Comparo el pattern obtenido con el que deberia ser
@@ -49852,14 +49856,18 @@ def convert_zero(a):
     ca = [0 if x==-1 else 1 for x in a]
     return ca
 
+def convert_bipolar(a):
+    ca = [-1 if x==0 else 1 for x in a]
+    return ca
+
 # TODO generar ruido aleatorio
 def add_noise(a):
     #for i in range(len(a)):
     #    if (i % 11) == 0:
     #        a[i] = 0
-    #a[1] = -1
+    a[1] = -1
     #a[2] = -1
-    #a[3] = -1
+    a[3] = -1
     #a[11] = -1
     #a[17] = -1
     #a[20] = -1

53b3e780856e2020d957bda66e8a36ea127acf59
J <jofuentes@est.frba.edu.ar>
2020-10-25 14:29:23 -0300

    Refactor de carga de bitmaps

--- katakana.py
+++ katakana.py
@@ -3,9 +3,20 @@ import traceback
 import sys 
 import datetime
 
-from training_data import init_train_images
-from training_data import init_test_images
 
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import os
+from tqdm import tqdm
+import seaborn as sns
+
+
+from load_data import init_train_images
+from load_data import init_test_images
+from load_data import get_test_set
+from load_data import get_pattern_length
+from load_data import flatten
 
 def main():
 
@@ -20,25 +31,23 @@ def main():
     
     
 
-    print("=>c032:")
-    print(train_images[0])
-    print("=>noisy_c032:")
-    print(test_images[0][0])
+#    print("=>c032:")
+#    print(train_images[0])
+#    print("=>noisy_c032:")
+#    print(test_images[0][0])
     
-    
-    # Size of image(width)
-    n_side = 64
     
     print("[LOG] Training -        " , datetime.datetime.now())    
     
     # No of neurons
-    n_neurons = n_side * n_side
+    n_neurons = get_pattern_length()
     W = train(n_neurons, train_images)
     
     # Test
     print("[LOG] Testing -         " , datetime.datetime.now())    
  
-    accuracy, op_imgs = test(W, test_images)
+    test_set = get_test_set()
+    accuracy, op_imgs = test(W, test_set)
 
     # Resultados
     print("[LOG] La precision de la red es %f" % (accuracy * 100))
@@ -57,16 +66,19 @@ def main():
 # neu: cantidad de elementos de cada vector de entrada
 # training data: vector de vectores de datos de entrada
 def train(neu, training_data):
+    #row, col = training_data.shape
     w = np.zeros([neu, neu])
     for data in training_data:
+        #display(data)
         w += np.outer(data, data)
     for diag in range(neu):
         w[diag][diag] = 0
-    print("Matriz de pesos:")
-    for row in w:
-        for val in row:
-            print(val)
-        print
+
+#    print("Matriz de pesos:")
+#    for row in w:
+#        for val in row:
+#            print(val)
+#        print
 
     return w
 
@@ -79,26 +91,18 @@ def test(weights, testing_data):
 
     for data in testing_data:
         true_data = data[0]
-        #noisy_data = np.array(data[1])
-        noisy_data = convert_to_binary(data[1])
+        noisy_data = data[1]
 
-        print("**************");
-        print("Con ruido:")
-        print(noisy_data)
-        print("Esperado:")
-        print(true_data)
-        
         # Utilizando la matriz W, ingreso con el pattern a reconocer
-        #predicted_data = retrieve_pattern(weights, noisy_data)
-        predicted_data = convert_to_binary(retrieve_pattern(weights, noisy_data))
+        p = retrieve_pattern(weights, noisy_data)
+        predicted_data = convert_to_binary(p)
         
         # Comparo el pattern obtenido con el que deberia ser
-        print("Obtenido:")
-        print(predicted_data)
-        print("**************");
         if np.array_equal(true_data, predicted_data):
             success += 1.0
-            print("=>Eureka!")
+            print("+Eureka!")
+        else:
+            print("-Failed");
         output_data.append([true_data, noisy_data, predicted_data])
 
     return (success / len(testing_data)), output_data
@@ -106,11 +110,11 @@ def test(weights, testing_data):
 
 # Function to retrieve individual noisy patterns
 def retrieve_pattern(weights, pattern, steps=10):
-    #res = np.array(pattern.copy())
-    res = pattern.copy()
-    #res = pattern
+    res = flatten(pattern)
 
-    for _ in range(steps):
+    print("res length = " + str(len(res)))
+    print("weights length = " + str(len(weights)))
+    for _ in tqdm(range(steps)):
         for i in range(len(res)):
             raw_v = np.dot(weights[i], res)
             if raw_v > 0:
@@ -129,6 +133,16 @@ def convert_to_bipolar(a):
     ca = [1 if x==1 else -1 for x in a]
     return ca
 
+def check(item):
+    res = [(type(item), len(item))]
+    for i in item:
+        res.append((type(i), (len(i) if hasattr(i, '__len__') else None)))
+    return res
+
+def display(pattern):
+    from pylab import imshow, cm, show
+    imshow(pattern.reshape((64,64)),cmap=cm.binary, interpolation='nearest')
+    show()
 
 if __name__ == "__main__":
     try: 
--- load_data.py
+++ load_data.py
@@ -8,7 +8,7 @@ test_path = "./bitmaps/test/"
 
 test_images = []
 train_images = []
-
+test_set = []
 
 
 def init_train_images():
@@ -17,17 +17,38 @@ def init_train_images():
 def init_test_images():
     return load_from_disk(test_path, test_images)
 
+def get_test_set():
+    for root, dirs, files in os.walk(train_path):
+        for filename in files:
+            # Open the image form working directory
+            train_image = Image.open(train_path + filename)
+            test_image = Image.open(test_path + filename)
+            train_data = np.asarray(train_image)
+            #train_data = flatten(train_image)
+            test_data = np.asarray(test_image)
+            #test_data = flatten(test_image)
+            test_set.append([train_data, test_data])
+    return test_set
+
+
 def load_from_disk(path, images_array):
     for root, dirs, files in os.walk(train_path):
         for filename in files:
-            images_array.append(load_image_as_array(path + filename))
+            # Open the image form working directory
+            image = Image.open(train_path + filename)
+            print("Loading " + train_path + filename + " Size:" + str(image.size))
+            data = np.asarray(image)
+            #data = flatten(image)
+            images_array.append(data)
     return images_array
-    
-def load_image_as_array(filename):
-    image = Image.open(filename)
-    return np.asarray(image)
-    
     
     
 def get_pattern_length():
-    return (64)
+    return (64*64)
+
+def flatten(m):
+    flat_pattern = []
+    for o in m:
+        for p in o:
+            flat_pattern.append(p)       
+    return np.array(flat_pattern)
